874f6dcda2b1638a8303360f7b49153f30778594
Ólafur Þór Gunnarsson <olinord@gmail.com>
2015-10-13 12:55:18 +0000

    windows fixes
    Fixed bootstrapping and running the app for windows machines

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,11 +2,17 @@ virtualenv python_virtualenv --no-site-packages
 
 call python_virtualenv/Scripts/activate.bat
 
-pip install pyopengl==3.1.0
+set PYSDL2_DLL_PATH=F:\games\GameSandbox-master
+
+pip install pyOpengl==3.1.0
 pip install pysdl2
+pip install numpy-1.10.0+mkl-cp27-none-win32.whl
 pip install pyrr
 pip install pyYaml
-pip install box2D
+pip install pypybox2d
+pip install nose
+pip install mock
+pip install imageio
 
 
 call python_virtualenv/Scripts/deactivate.bat
---
+++ runGame.bat
@@ -0,0 +1,1 @@
+python_virtualenv\Scripts\python.exe -B src\main.py
